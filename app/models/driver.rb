347c704756811152f7c5dab3dd384b7f09be6eda
Charles Ellis <hamled@hamled.name>
2017-03-30 20:58:14 -0700

    Active and available scopes for Driver
    The active scope returns all Drivers that are currently online, while
    the available scope returns all Drivers that are online and are not
    associated with any ongoing trips.

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,6 +1,21 @@
 class Driver < ApplicationRecord
   has_many :trips
 
+  scope :active, -> { where(active: true) }
+
+  def self.available
+    # I tried for hours but could not find a way to do this
+    # without resorting to raw SQL with the condition from `ongoing`
+    # hard-coded. Arel could also be used, but has the same problem
+    # with needing to be hard-coded AFAICT.
+    #
+    # This should be fine for the students to write, but it does
+    # result in between n and 2n DB queries, where n is active drivers
+    active.select do |d|
+      d.trips.count == 0 || d.trips.ongoing.count == 0
+    end
+  end
+
   def average_rating
     return 1
   end
