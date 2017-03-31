666fd4f1bfc0e034ad28f53aa514c8fe33a1af82
Charles Ellis <hamled@hamled.name>
2017-03-30 21:01:16 -0700

    Passengers logic for requesting a ride
    This method raises exceptions if the Passenger is not in a valid state
    to request a ride (they're already in an ongoing trip).
    
    Otherwise, a new Trip is created with this Passenger and a random
    available Driver. Nil is returned if no Drivers could be found to
    fulfill the request.

---
+++ app/errors/ride_request_error.rb
@@ -0,0 +1,2 @@
+class RideRequestError < StandardError
+end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,18 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  def can_request?
+    trips.where.not(status: :complete).count == 0
+  end
+
+  def request_ride!
+    # Check if we can request a new ride
+    raise RideRequestError.new("Passenger cannot currently request rides") unless can_request?
+
+    # Find an available driver and create a trip with them, if possible
+    driver = Driver.available.sample
+    trips.create(passenger: self,
+                 driver: driver,
+                 date: Date.today) if driver
+  end
 end
