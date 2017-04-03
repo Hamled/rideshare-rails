cadf492d816dcae8cdc8f70fa87c38f57fb573d1
Charles Ellis <hamled@hamled.name>
2017-04-03 12:02:18 -0700

    Drivers are always available when active

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,19 +2,7 @@ class Driver < ApplicationRecord
   has_many :trips
 
   scope :active, -> { where(active: true) }
-
-  def self.available
-    # I tried for hours but could not find a way to do this
-    # without resorting to raw SQL with the condition from `ongoing`
-    # hard-coded. Arel could also be used, but has the same problem
-    # with needing to be hard-coded AFAICT.
-    #
-    # This should be fine for the students to write, but it does
-    # result in between n and 2n DB queries, where n is active drivers
-    active.select do |d|
-      d.trips.count == 0 || d.trips.ongoing.count == 0
-    end
-  end
+  scope :available, -> { active }
 
   def average_rating
     return 1
