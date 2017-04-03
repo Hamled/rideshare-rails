ca649c52847445ca1a1c792ded11b2d74e998540
Charles Ellis <hamled@hamled.name>
2017-04-03 11:54:55 -0700

    Remove trip action buttons from driver show page

--- app/helpers/drivers_helper.rb
+++ app/helpers/drivers_helper.rb
@@ -10,22 +10,4 @@ module DriversHelper
 
     button_to text, activate_driver_path(driver), method: :post, class: style
   end
-
-  def driver_trip_action_button(driver)
-    return unless driver.current_trip
-
-    case driver.current_trip.status
-    when 'pickup'
-      text = 'Confirm pickup'
-      action = :pickup
-    when 'travel'
-      text = 'Confirm dropoff'
-      action = :dropoff
-    else
-      return
-    end
-
-    button_to text, { controller: 'drivers', action: action, driver: driver },
-                    { method: :post, class: 'button success' }
-  end
 end
